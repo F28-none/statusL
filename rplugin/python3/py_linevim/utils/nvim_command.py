0b799bb6c1395a631f34c5e3d3f78e279bcd7764
F28-none <fadlidev99@gmail.com>
2025-06-14 13:20:19 +0700

    try execp in render

--- rplugin/python3/py_linevim/utils/nvim_command.py
+++ rplugin/python3/py_linevim/utils/nvim_command.py
@@ -1,13 +1,17 @@
 #menerapkan semua yg komponen ke statusline neovim
 def render(nvim,parts,highlight):
     #build status 
-    status = []
-    for key,valtype in parts.items():
-        for part,value in valtype.items():
-            status.append(value)
-    statusline = ''.join(status)
+    try:
+        status = []
+        for key,valtype in parts.items():
+            for part,value in valtype.items():
+                status.append(value)
+        statusline = ''.join(status)
     #highlight status
-    nvim.command(f'set statusline={statusline}')
-    for group,color in highlight.items():
-       nvim.command(f'highlight {group} {color}')
+        nvim.command(f'set statusline={statusline}')
+        for group,color in highlight.items():
+           nvim.command(f'highlight {group} {color}')
+    except Exception as e:
+        nvim.command(f'eror saat build status {e}')
+
 
