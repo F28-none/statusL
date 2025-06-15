683bd2448a56e979a3f70a819c549e22402a86cf
F28-none <fadlidev99@gmail.com>
2025-06-15 14:09:19 +0700

    refactor code

--- rplugin/python3/py_linevim/__init__.py
+++ rplugin/python3/py_linevim/__init__.py
@@ -11,33 +11,44 @@ class PyLine:
         self.nvim = nvim
         self.parts = Parts()
 
-    #function for handel user configuration
+    """
+    Fungsi ini di gunakan untuk menagani Konfigurasi dari user
+    """
     @function('Py_line_config',sync=True)
     def user_config(self,args):
+        """
+        Pengecheckan untuk memvalidasi Konfigurasi Harus sebuah dict
+        """
         try:
             if not args or not isinstance(args[0], dict):
                 raise ValueError('Konfigurasi harus dictionary')
-            data = args[0]
+            config = args[0]
             #only user can config
-            mode_data = data.get('mode_bg',{})
-            key_user = mode_data.keys()
+            config_mode = config.get('mode_bg',{})
+            key_user = config_mode.keys()
+            """
+            Melakukan Pengecheckan ,key yang di kirim harus valid dengan key dari base
+            """
             key_base = self.parts.mode_bg.keys()
             for key in key_user:
                 if not key in key_base:
                     raise ValueError(f'maaf mode {key} bukan mode nvim') 
-            self.parts.mode_bg = mode_data
-            self.parts.section_2= data.get('file_bg',self.parts.section_2)
-            self.parts.section_3= data.get('branch_bg',self.parts.section_3)
-            self.parts.pipe= data.get('pipe',self.parts.pipe)
-            self.parts.icon_file= data.get('icon_file',self.parts.icon_file)
-            self.parts.icon_branch= data.get('icon_branch',self.parts.icon_branch)
-            self.parts.shape_left= data.get('separator_left',self.parts.shape_left)
-            self.parts.shape_right= data.get('separator_right',self.parts.shape_right)
-            self.parts.icon_mode= data.get('icon_mode',self.parts.icon_mode)
+
+            self.parts.mode_bg =config_mode 
+            self.parts.section_2= config.get('file_bg',self.parts.section_2)
+            self.parts.section_3= config.get('branch_bg',self.parts.section_3)
+            self.parts.separator= config.get('separator',self.parts.separator)
+            self.parts.icon_file= config.get('icon_file',self.parts.icon_file)
+            self.parts.icon_branch= config.get('icon_branch',self.parts.icon_branch)
+            self.parts.border_left= config.get('border_left',self.parts.border_left)
+            self.parts.border_right= config.get('border_right',self.parts.border_right)
+            self.parts.icon_mode= config.get('icon_mode',self.parts.icon_mode)
         except Exception as e:
             self.nvim.command(f'echo"[PyLine Error] Config gagal: {e}\n"')
         
-    #set pertama kali saat di membuka neovim
+    """
+    jalankan pertamakali neovim di buka
+    """
     @autocmd('VimEnter',sync=True)
     def on_open(self):
         try:
@@ -53,32 +64,47 @@ class PyLine:
     @autocmd('ModeChanged,BufWritePost',pattern='*',sync=True)
     def set_status_line(self):
         mode_core = self.get_mode_nvim()
+        """
+        mode core yg di dapatkan dari neovim,
+        hanya karakter n,i,v dll.
+        di sini saya memfilter agar mode lebih informatif,
+        contoh : n >> Normal
+        """
         mode = info_mode(mode_core)
+
         branch_name = get_branch_name()
         info_branch = get_branch_info()
 
+        """
+        Jika directori saat nvim di buka,
+        bukan .git atau git repo akan di gantikan dengan syimbol di bawah
+        """
         if not branch_name:
             branch_name = ''
 
         #separator color for integrated with mode
         #section 1
+        """
+        sepesial border untuk section mode,
+        agar border selalu mengitu warna bg dan fg dari mode
+        """
         mode_color = send_color(get_color_mode(mode_core,self.parts.mode_bg))
-        shape_color_mode = send_color('#FF000000',get_color_mode(mode_core,self.parts.mode_bg))
+        border_color_mode = send_color(self.parts.default_border_bg,get_color_mode(mode_core,self.parts.mode_bg))
 
         #section 2 
         file_color = send_color(self.parts.section_2)
-        shape_color_file = send_color('#FF000000',self.parts.section_2)
+        border_color_file = send_color(self.parts.default_border_bg,self.parts.section_2)
 
         #section 3
-        shape_color_branch = send_color('#FF000000',self.parts.section_3)
+        border_color_branch = send_color(self.parts.default_border_bg,self.parts.section_3)
         branch_color = send_color(self.parts.section_3)
 
         highlight = {
             'branch':branch_color,
             'mode':mode_color,
-            'shapeMode':shape_color_mode,
-            'shapeBranch':shape_color_branch,
-            'shapeFile':shape_color_file,
+            'borderMode':border_color_mode, #sepesial group untuk border mode
+            'borderBranch':border_color_branch,
+            'borderFile':border_color_file,
             'file':file_color,
         }
 
@@ -88,12 +114,12 @@ class PyLine:
         bukan caracter seperti |
         """
         style_status = {
-            'pipe2':{
-                'pipe2':f'\\{self.parts.pipe}',
+            'separator2':{
+                'separator2':f'\\{self.parts.separator}',
             },
-            'shape_left1':{
-                'shape_group':'%#shapeMode#',
-                'shape':self.parts.shape_left,
+            'border_left1':{
+                'border_group':'%#borderMode#',
+                'border':self.parts.border_left,
             },
             'nv_icon':{
                 'mode_group':'%#mode#',
@@ -103,34 +129,34 @@ class PyLine:
                 'mode_group':'%#mode#',
                 'mode_part':mode,
             },
-            'shape_left2':{
-                'shape_group':'%#shapeMode#',
-                'shape':self.parts.shape_right,
+            'border_left2':{
+                'border_group':'%#borderMode#',
+                'border':self.parts.border_right,
             },
-            'pipe3':{
-                'pipe_group':'%#pipe#',
-                'pipe3':f'\\{self.parts.pipe}',
+            'separator3':{
+                'separator_group':'%#separator#',
+                'separator3':f'\\{self.parts.separator}',
             },
-            'shape_left3':{
-                'shape_group':'%#shapeFile#',
-                'shape':self.parts.shape_left,
+            'border_left3':{
+                'border_group':'%#borderFile#',
+                'border':self.parts.border_left,
             },
             'file':{
                 'file_group':'%#file#',
                 'file_icon':self.parts.icon_file,
-                'file_part':self.parts.filename,
+                'file_part':self.parts.file_name,
             },
-            'shape_left4':{
-                'shape_group':'%#shapeFile#',
-                'shape':self.parts.shape_right,
+            'border_left4':{
+                'border_group':'%#borderFile#',
+                'border':self.parts.border_right,
             },
-            'pipe4':{
-                'pipe_group':'%#pipe#',
-                'pipe1':f'\\{self.parts.pipe}',
+            'separator4':{
+                'separator_group':'%#separator#',
+                'separator1':f'\\{self.parts.separator}',
             },
-            'shape_left6':{
-                'shape_group':'%#shapeBranch#',
-                'shape':self.parts.shape_left,
+            'border_left6':{
+                'border_group':'%#borderBranch#',
+                'border':self.parts.border_left,
             },
             'branch':{
                 'branch_group':'%#branch#',
@@ -145,13 +171,13 @@ class PyLine:
             'row_col':{
                 'row_col_part':self.parts.row_col,
             },
-            'shape_left5':{
-                'shape_group':'%#shapeBranch#',
-                'shape':self.parts.shape_right,
+            'border_left5':{
+                'border_group':'%#borderBranch#',
+                'border':self.parts.border_right,
             },
-            'pipe1':{
-                'pipe_group':'%#pipe#',
-                'pipe1':f'\\{self.parts.pipe}',
+            'separator1':{
+                'separator_group':'%#separator#',
+                'separator1':f'\\{self.parts.separator}',
             },
         }
         render(self.nvim,style_status,highlight)
--- rplugin/python3/py_linevim/utils/colors.py
+++ rplugin/python3/py_linevim/utils/colors.py
@@ -1,7 +1,12 @@
-#mendefiniikan dan membuat configurasi color
+"""
+file ini mengatur semua highlight atau color,
+menggunakan default color jika tidak ada yg di kirim
+"""
 def send_color(bg='#000000',fg='#ffffff',font='bold'):
     return f'guibg={bg} guifg={fg} gui={font}'
 
-#mngubah warna di setiao masing masing mode
+"""
+sepesial untuk mengubah color setiap mode
+"""
 def get_color_mode(mode,mode_theme):
         return mode_theme.get(mode,'#000000')
