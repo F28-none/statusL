ce02468435bbfb74f30f5802bd100c12d1c5b484
F28-none <fadlidev99@gmail.com>
2025-06-26 22:43:43 +0700

    change color default to black

--- rplugin/python3/py_linevim/utils/parts.py
+++ rplugin/python3/py_linevim/utils/parts.py
@@ -2,23 +2,23 @@
 class Parts:
     def __init__(self) -> None:
         self.mode_bg = {
-                'Normal':'#6f03fc',
+                'Normal':'#000000',
                 'Insert':'#000000',
-                'Visual':'#888888',
-                'VisualLine':'#888888',
-                'VisualBlock':'#888888',
+                'Visual':'#000000',
+                'VisualLine':'#000000',
+                'VisualBlock':'#000000',
                 'Command':'#000000',
-                'Replace':'#888888',
-                'Terminal':'#888888',
-                'Select':'#888888',      
+                'Replace':'#000000',
+                'Terminal':'#000000',
+                'Select':'#000000',      
                 }
         self.icon_file = '󱔘'
         self.icon_branch = ''
         self.file_name = '%t%m'
         self.row_col= '%l:%c'
-        self.section_1 = '#6f03fc'
-        self.section_2 = '#6f03fc'
-        self.section_3= '#6f03fc'
+        self.section_1 = '#000000'
+        self.section_2 = '#000000'
+        self.section_3= '#000000'
         self.font_weight = 'NONE'
         self.make_coloum = '%='
         self.border_right = ''
